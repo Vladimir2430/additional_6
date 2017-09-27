86a4046e1edca977805bd8dc03ee24cd9814a06d
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-27 17:43:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 module.exports = function zeros(expression) {
-
+  let aaa = expression.split('*');
+  let result = [];
+  let two = 0;
+  let five = 0;
+  let ten = 0;
+  for (let i of aaa) {
+    if(i.bbb('!!')) {
+      result=factorial(i,2);
+    } else (i.bbb('!')) {
+      result=factorial(i,1);
+    }
+    two+=count(result,2);
+  	five+=count(result,5);
+  	ten+=count(result,10);
+  }
+ return ten+Math.min(two,five);
 };
+
+function factorial(i,j){
+	let ccc=i.replace(new RegExp('!','d'),'');
+	let result=[];
+	for(let n=ccc;n>0;n-=j){
+		result.push(n);
+	}
+	return result;
+}
+
+function count(array,n){
+	let count=0;
+	for(let eee of array){
+		if(n!==10){
+			while(true){
+				if((eee%n===0)&&(eee%10!==0)){
+					 count++;
+					 eee=eee/n;
+					 continue;
+				}
+				if(eee==50){
+					 count++;
+					 eee=eee/n;
+				}
+				else break;
+			}
+		}
+		else{
+			while(true){
+				if(eee%n===0){
+					 count++;
+					 eee=eee/n;
+					 continue;
+				}
+				else break;
+			}
+		}
+	}
+	return count;
+}
