8b87933e27514fa3864e07e2ce0cbbcc4ab52527
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-23 11:18:00 +0300

    code improved

--- src/index.js
+++ src/index.js
@@ -1,57 +1,30 @@
 module.exports = function zeros(expression) {
-  let aaa = expression.split('*');
-  let result=[];
-  let two = 0;
-  let five = 0;
-  let ten = 0;
-  for (let i of aaa) {
-    if(i.endsWith('!!')) {
-      result=factorial(i,2);
-    } else if (i.endsWith('!')) {
-      result=factorial(i,1);
-    }
-    two+=count(result,2);
-    five+=count(result,5);
-    ten+=count(result,10);
+  let arr = expression.split('*'), result = [], two = 0, five = 0, ten = 0;
+  for (let i of arr) {
+    i.endsWith('!!') ? result = factorial(i, 2) : result = factorial(i, 1);
+    two  += count(result, 2);
+    five += count(result, 5);
+    ten  += count(result, 10);
   }
- return ten+Math.min(two,five);
+  return two > five ? ten + five : ten + two;
 };
 
-function factorial(i,j){
-	let ccc=i.replace(new RegExp('!','g'),'');
-	let result=[];
-	for(let n=ccc;n>0;n-=j){
-		result.push(n);
+function factorial(k, j) {
+	let arr = k.replace(new RegExp('!', 'g'), ''), result = [];
+	for (let i = arr; i > 0; i -= j) {
+		result.push(i);
 	}
 	return result;
 }
 
-function count(arr,n){
-	let count=0;
-	for(let eee of arr){
-		if(n!==10){
-			while(true){
-				if((eee%n===0)&&(eee%10!==0)){
-					 count++;
-					 eee/=n;
-					 continue;
-				}
-				if(eee==50){
-					 count++;
-					 eee/=n;
-				}
-				else break;
-			}
-		}
-		else{
-			while(true){
-				if(eee%n===0){
-					 count++;
-					 eee/=n;
-					 continue;
-				}
-				else break;
-			}
+function count(arr, n) {
+	let count = 0;
+	for(let i of arr) {
+		while (n !== 10) {
+			if ((i % n === 0) && (i % 10 !== 0) || i == 50 || i % n === 0) {
+				count++;
+				i /= n;
+			} else break;
 		}
 	}
 	return count;
